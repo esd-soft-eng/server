b25bb21e51590e0a50412f5e20e47d328dbb289f
Ske <Ske@Ske-PC>
2013-02-16 15:24:54 +0000

    MD5 login functionality fixed

--- MuseumServer/src/java/utility/MD5Hasher.java
+++ MuseumServer/src/java/utility/MD5Hasher.java
@@ -1,30 +1,32 @@
 package utility;
 
 import java.security.*;
-import java.math.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
- * @author Alex
+ * @author Alex, Simon
  */
 public class MD5Hasher {
-    
-    public static String hashMD5(String hash) {
-        
-        String returnedHash = "";
-        
+
+    public static String hashMD5(String hash) throws NoSuchAlgorithmException {
+
+        byte[] inputBytes = hash.getBytes();
+        String output = null;
+
         try {
-            MessageDigest m = MessageDigest.getInstance("MD5");
-            m.update(hash.getBytes(),0,hash.length());
-            returnedHash = new BigInteger(0,m.digest()).toString(16);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.reset();
+            md.update(inputBytes);
+            byte msgDigest[] = md.digest();
+
+            StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < msgDigest.length; i++) {
+                hexString.append(Integer.toHexString(
+                        (msgDigest[i] & 0xFF) | 0x100).toLowerCase().substring(1, 3));
+            }
+            output = hexString.toString();
         } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(MD5Hasher.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        System.out.println(returnedHash);
-        
-        return returnedHash;
+        return output;
     }
 }
