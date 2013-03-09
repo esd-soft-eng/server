6c905a34275e886075845e4ef58fb37e7ab026e3
Darkstar <Darkstar@Darkstar-PC>
2013-03-09 17:09:28 +0000

    Sql update fix

--- MuseumServer/museum_server_database.sql
+++ MuseumServer/museum_server_database.sql
@@ -176,16 +176,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `tours`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tours` (
-  `TourID` int(11) NOT NULL AUTO_INCREMENT,
-  `TourName` varchar(50) NOT NULL,
-  `TourDescription` varchar(200) NOT NULL,
-  PRIMARY KEY (`TourID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tours`
 ----
