bd5d1a1cc7a7519ab3909f4c45b94b84ddafc939
Oliver Brooks <oliver2.brooks@live.uwe.ac.uk>
2013-03-09 12:34:00 +0000

    Remove router completed

--- MuseumServer/src/java/businessDomainObjects/RouterManager.java
+++ MuseumServer/src/java/businessDomainObjects/RouterManager.java
@@ -1,9 +1,44 @@
 package businessDomainObjects;
 
+import java.util.ArrayList;
+import persistance.PersistanceRepositoryRouter;
+
 /**
  *
  * @author Oliver Brooks <oliver2.brooks@live.uwe.ac.uk>
  */
 public class RouterManager {
-    
+
+    private PersistanceRepositoryRouter persistance;
+    private ArrayList<Router> listOfRouters;
+
+    public RouterManager(PersistanceRepositoryRouter pr) {
+        this.persistance = pr;
+        listOfRouters = persistance.getAllRouters();
+    }
+
+    public synchronized boolean removeRouter(String MAC) {
+        if (MAC == null || MAC == "") {
+            return false;
+        }
+
+        if (getRouterByMAC(MAC) == null) {
+            return false;
+        }
+
+        if (!persistance.removeRouter(MAC)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private Router getRouterByMAC(String MAC) {
+        for (Router r : listOfRouters) {
+            if (r.getMACAddress() == MAC) {
+                return r;
+            }
+        }
+        return null;
+    }
 }
